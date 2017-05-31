de39c05e50a1787fa1cc127097a60d8d7bb1118c
Nick Anthony <nicholas_anthony@brown.edu>
2017-05-31 12:28:45 -0700

    Changed php to file_get_contents call

--- local.php
+++ local.php
@@ -2,8 +2,6 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-type: application/json');
 $url = 'http://www.gotuftsjumbos.com/sports/fball/index';
-$ch =  curl_init($url.'?'.$_SERVER['QUERY_STRING']);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$result = curl_exec($ch);
-echo $result;
+$output = file_get_contents($url); 
+echo $output;
 ?>
